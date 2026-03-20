6261e1be71db524db1bb7fd1f340c70b9ad265e1
Julien Cotte <129265577+Fenriz1349@users.noreply.github.com>
2026-03-20 15:23:22 +0100

    add userRemote, userRemoteDataSource and anonymous sign in for test

--- EchoLedger/App/Navigation/ContentView.swift
+++ EchoLedger/App/Navigation/ContentView.swift
@@ -9,15 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack {
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
+        Text("EchoLedger")
+            .task {
+                do {
+                    let dataSource = UserRemoteDataSource()
+                    let uid = try await dataSource.signInAnonymously()
+                    print("✅ User anonyme : \(uid)")
+                } catch {
+                    print("❌ Erreur : \(error)")
+                }
             }
-            .padding()
-        }
     }
 }
 
